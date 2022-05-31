695a67469118115234ed77b4070654c721a063c3
DCherish <jongillou@gmail.com>
2022-05-31 19:44:59 +0900

    stuDy

---
+++ Day04/SpotifyLogin/SpotifyLogin/MainViewController.swift
@@ -0,0 +1,53 @@
+//
+//  MainViewController.swift
+//  SpotifyLogin
+//
+//  Created by kid cherish on 2022/05/31.
+//
+
+import UIKit
+import FirebaseAuth
+
+class MainViewController: UIViewController {
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var resetPasswordButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.navigationBar.isHidden = true
+        
+        let email = Auth.auth().currentUser?.email ?? "고객"
+        
+        welcomeLabel.text = """
+        환영합니다.
+        \(email)님
+        """
+        
+        let isEmailSignIn = Auth.auth().currentUser?.providerData[0].providerID == "password"
+        
+        resetPasswordButton.isHidden = !isEmailSignIn
+    }
+    
+    @IBAction func logoutButtonTapped(_ sender: UIButton) {
+        let firebaseAuth = Auth.auth()
+        
+        do {
+            try firebaseAuth.signOut()
+            self.navigationController?.popToRootViewController(animated: true)
+        } catch let signoutError as NSError {
+            print("Error: sign out \(signoutError.localizedDescription)")
+        }
+    }
+    
+    @IBAction func resetPasswordButtonTapped(_ sender: UIButton) {
+        let email = Auth.auth().currentUser?.email ?? ""
+        Auth.auth().sendPasswordReset(withEmail: email, completion: nil)
+    }
+}
