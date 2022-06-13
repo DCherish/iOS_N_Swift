3a5884e7cd26cea079ae2f8282e4afa32c05a18f
DCherish <jongillou@gmail.com>
2022-06-13 16:48:25 +0900

    stuDy

---
+++ Day11/RxSwiftPractice/FilteringOperator.playground/Contents.swift
@@ -0,0 +1,131 @@
+import RxSwift
+
+let disposeBag = DisposeBag()
+
+print("-----ignoreElement-----")
+let sleepMode = PublishSubject<String>()
+
+sleepMode
+    .ignoreElements()
+    .subscribe {
+        print($0)
+    }
+    .disposed(by: disposeBag)
+
+sleepMode.onNext("🔈")
+sleepMode.onNext("🔈")
+sleepMode.onNext("🔈")
+
+sleepMode.onCompleted()
+
+print("-----elementAt-----")
+let twiceAlertWakeUp = PublishSubject<String>()
+
+twiceAlertWakeUp
+    .element(at: 2)
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+twiceAlertWakeUp.onNext("🔈")
+twiceAlertWakeUp.onNext("🔈")
+twiceAlertWakeUp.onNext("😳")
+twiceAlertWakeUp.onNext("🔈")
+
+print("-----Filter-----")
+Observable.of(1, 2, 3, 4, 5, 6, 7, 8)
+    .filter { $0 % 2 == 0 }
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+print("-----Skip-----")
+Observable.of("😆", "🥹", "🙂", "😝", "🥳", "😣", "😵‍💫")
+    .skip(5)
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+print("-----SkipWhile-----")
+Observable.of("😆", "🥹", "🙂", "😝", "🥳", "😣", "🤫", "😲")
+    .skip(while: {
+        $0 != "😣"
+    })
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+print("-----SkipUntil-----")
+let customer = PublishSubject<String>()
+let openTime = PublishSubject<String>()
+
+customer
+    .skip(until: openTime)
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+customer.onNext("😒")
+customer.onNext("🥸")
+
+openTime.onNext("땡!")
+customer.onNext("🤭")
+
+print("-----Take-----")
+Observable.of("🥇", "🥈", "🥉", "🥴", "😯")
+    .take(3)
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+print("-----TakeWhile-----")
+Observable.of("🥇", "🥈", "🥉", "🥴", "😯")
+    .take(while: {
+        $0 != "🥉"
+    })
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+print("-----enimerated-----")
+Observable.of("🥇", "🥈", "🥉", "🥴", "😯")
+    .enumerated()
+    .take(while: {
+        $0.index < 3
+    })
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+print("-----TakeUntil-----")
+let lecture = PublishSubject<String>()
+let end = PublishSubject<String>()
+
+lecture
+    .take(until: end)
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+lecture.onNext("🙋🏼‍♀️")
+lecture.onNext("🙋🏻")
+
+end.onNext("끝!")
+lecture.onNext("🙋🏼‍♂️")
+
+print("-----DistinctUntilChanged-----")
+Observable.of("저는", "저는", "앵무새", "앵무새", "입니다", "입니다", "저는", "저는", "앵무새", "앵무새", "입니다", "입니다", "저는", "저는", "앵무새", "앵무새", "일까요?", "일까요?", "일까요?", "일까요?", "저는", "저는", "앵무새", "앵무새", "입니다", "입니다")
+    .distinctUntilChanged()
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
---
+++ Day11/RxSwiftPractice/TransformingOperators.playground/Contents.swift
@@ -0,0 +1,167 @@
+import RxSwift
+
+let disposeBag = DisposeBag()
+
+print("-----toArray-----")
+Observable.of("A", "B", "C")
+    .toArray()
+    .subscribe(
+        onSuccess: {
+            print($0)
+        }
+    )
+    .disposed(by: disposeBag)
+
+print("-----Map-----")
+Observable.of(Date())
+    .map { date -> String in
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        dateFormatter.locale = Locale(identifier: "ko_KR")
+        return dateFormatter.string(from: date)
+    }
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+print("-----FlatMap-----")
+protocol athlete {
+    var score: BehaviorSubject<Int> { get }
+}
+
+struct archer: athlete {
+    var score: BehaviorSubject<Int>
+}
+
+let koreaNA = archer(score: BehaviorSubject<Int>(value: 10))
+let usaNA = archer(score: BehaviorSubject<Int>(value: 8))
+
+let olympic = PublishSubject<athlete>() // 중첩된 Observable
+
+olympic
+    .flatMap { athlete in
+        athlete.score
+    }
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+olympic.onNext(koreaNA)
+koreaNA.score.onNext(10)
+
+olympic.onNext(usaNA)
+koreaNA.score.onNext(10)
+usaNA.score.onNext(9)
+
+print("-----FlatMapLatest-----")
+struct highjumper: athlete {
+    var score: BehaviorSubject<Int>
+}
+
+let seoulA = highjumper(score: BehaviorSubject<Int>(value: 7))
+let jejuA = highjumper(score: BehaviorSubject<Int>(value: 6))
+
+
+let champs = PublishSubject<athlete>()
+
+champs
+    .flatMapLatest { athlete in
+        athlete.score
+    }
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+champs.onNext(seoulA)
+seoulA.score.onNext(9)
+
+champs.onNext(jejuA) // 이제 제주 점수 시퀀스 변경 집중 (최신)
+seoulA.score.onNext(10) // 제주로 변경되며 서울은 해제되어 변경되어도 출력 X
+jejuA.score.onNext(8)
+jejuA.score.onNext(7)
+
+print("-----Materialize and Dematerialize-----")
+enum foul: Error {
+    case earlyStart
+}
+
+struct runner: athlete {
+    var score: BehaviorSubject<Int>
+}
+
+let kim = runner(score: BehaviorSubject<Int>(value: 0))
+let park = runner(score: BehaviorSubject<Int>(value: 1))
+
+let meter100 = BehaviorSubject<athlete>(value: kim)
+
+meter100
+    .flatMapLatest { athlete in
+        athlete.score
+            .materialize()
+    }
+    .filter {
+        guard let error = $0.error else {
+            return true
+        }
+        print(error)
+        return false
+    }
+    .dematerialize()
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+kim.score.onNext(1)
+kim.score.onError(foul.earlyStart)
+kim.score.onNext(2)
+
+meter100.onNext(park)
+
+print("-----Example(phoneNumber11)-----")
+let input = PublishSubject<Int?>()
+
+let list: [Int] = [1]
+
+input
+    .flatMap {
+        $0 == nil ? Observable.empty() : Observable.just($0)
+    }
+    .map { $0! }
+    .skip(while: { $0 != 0 })
+    .take(11)
+    .toArray()
+    .asObservable()
+    .map {
+        $0.map { "\($0)" }
+    }
+    .map { numbers in
+        var numberList = numbers
+        numberList.insert("-", at: 3)
+        numberList.insert("-", at: 8)
+        
+        let number = numberList.reduce(" ", +)
+        return number
+    }
+    .subscribe(onNext: {
+        print($0)
+    })
+    .disposed(by: disposeBag)
+
+input.onNext(10)
+input.onNext(0)
+input.onNext(nil)
+input.onNext(1)
+input.onNext(0)
+input.onNext(4)
+input.onNext(3)
+input.onNext(nil)
+input.onNext(1)
+input.onNext(8)
+input.onNext(9)
+input.onNext(4)
+input.onNext(9)
+input.onNext(1)
